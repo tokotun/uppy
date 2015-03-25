31bce05f799f2b0ae5a6834d4f0dbeff63419257
tokotun <saar.2012@yandex.ru>
2015-03-25 23:44:43 +0300

    Russian loading files made

--- uppy/app/functions.php
+++ uppy/app/functions.php
@@ -1,11 +1,12 @@
 <?php
     function fileForceDownload(\Uppy\file $file, $uploadPath) {
-    	$path = $uploadPath . $file->id . '_' . $file->name;
-    	$path_to_somefile = realpath($path);
+    	$path = $uploadPath . $file->getNameForSave();
+    	$path_to_somefile = $file->id . '_' . $file->name;
+
 	  	if (file_exists($path)) {
 	  	    header("X-SendFile:  $path_to_somefile");
 		  	header("Content-Type: application/octet-stream");
-		  	header("Content-Disposition: attachment; filename*=\"utf8'ru-ru'$file->name\"");
+		  	header("Content-Disposition: attachment;");
 		   	exit;
   		}
   	}
