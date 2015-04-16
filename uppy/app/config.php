7b72fdeb0a0765f8ebe3b686ceb66b05939a39d6
tokotun <saar.2012@yandex.ru>
2015-04-16 23:47:58 +0300

    readme describe config

--- uppy/app/config.php
+++ uppy/app/config.php
@@ -1,6 +1,6 @@
 <?php 
     $uploadPath = 'uppy/container/'; //путь к папке с хранимыми файлами
-    $maxFileSize = 8388608;      // 8 MB . Максимальный размер принимаемых файлов.
+    $maxFileSize = 16777216;      // 8 MB . Максимальный размер принимаемых файлов.
     $hostName = 'http://localhost/uppy';  
     $dbHost = 'localhost'; //имя базы данных
     $dbUser = 'root';      //имя пользователя базы данных
